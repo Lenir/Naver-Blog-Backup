68c22ee9ca07384c9f4567d395dc3c37a1ddf942
Lenir <1net1@naver.com>
2017-12-13 18:42:39 +0900

    make methods related files, dirs
    downloading img, making backup directory

--- NaverBlogCrawler.py
+++ NaverBlogCrawler.py
@@ -1,20 +1,40 @@
 from bs4 import BeautifulSoup
 import requests
+from urllib import request
+import os
 import re
 
 class NaverBlogCrawler:
     def __init__(self, url):
         self.url = url
-        self.postFrameUrl = self.getPostFrameUrl()
+        self.postFrameUrl = ""
+        self.postTitle = ""
+        self.postNumber = ""
+        self.postSoup = None
+        self.postFrameSoup = None
+        self.backupDir = ""
+        self.postSetup()
 
     def isSmartEditor3Posting(self, postFrameUrl):
         pass
 
-    def getPostFrameUrl(self):
+    def postSetup(self):
         postHtmlSource = requests.get(self.url).text
-        postSoup = BeautifulSoup(postHtmlSource, "html5lib")
+        self.postSoup = BeautifulSoup(postHtmlSource, "html5lib")
+        self.postFrameUrl = self.getPostFrameUrl()
+        self.postNumber = self.getPostNumber()
 
-        mainFrameTag = postSoup.find('frame', {'id': 'mainFrame'})
+
+        postFrameHttpResponse = requests.get(self.postFrameUrl).text
+        self.postFrameSoup = BeautifulSoup(postFrameHttpResponse, "html5lib")
+        self.postTitle = self.getPostTitle()
+
+        self.backupDir = self.getBackupDirName()
+
+        self.makeBackupDir()
+
+    def getPostFrameUrl(self):
+        mainFrameTag = self.postSoup.find('frame', {'id': 'mainFrame'})
         mainFrameAttr = str(mainFrameTag).split(' ')
 
         postUrlSuffix = ""
@@ -27,13 +47,30 @@ class NaverBlogCrawler:
 
         return postFrameUrl
 
+    def getPostNumber(self):
+        postNum = re.sub(".*naver.com/.*/", '', self.url)
+        return postNum
+
+    def makeBackupDir(self):
+        os.makedirs(self.backupDir, exist_ok=True)
+
+    # post backup will save in ./posts/{postnumber+posttitle}
+    # eg. 221156999402[자료구조] 트라이(Trie)
+    def getBackupDirName(self):
+        dirName = "posts/"
+        return dirName + self.postNumber + self.postTitle
+
+    def getPostTitle(self):
+        titleTag = self.postFrameSoup.find('title')
+        title = re.sub("<title>", '', str(titleTag))
+        title = re.sub(" : 네이버 블로그</title>", '', title)
+        return title
+
     def getPostEditAreas(self):
         if self.postFrameUrl is None:
             raise InvalidUrl
         else:
-            postBodyHttpResponse = requests.get(self.postFrameUrl).text
-            postBodySoup = BeautifulSoup(postBodyHttpResponse, "html5lib")
-            editAreas = postBodySoup.find_all('div',{'class':'se_editArea'})
+            editAreas = self.postFrameSoup.find_all('div',{'class':'se_editArea'})
             return editAreas
 
     def isTextEditArea(self, editArea):
@@ -42,6 +79,15 @@ class NaverBlogCrawler:
             return True
         else:
             return False
+
+    def saveImage(self, imageUrl, saveLoc):
+        saveName = self.getSaveImageName(imageUrl)
+        request.urlretrieve(imageUrl, saveLoc + saveName)
+
+    def getSaveImageName(self, imageUrl):
+        fname = re.search('net/.*(PNG|JPEG|GIF)', imageUrl).group()
+        fname = re.sub('[/]', '', fname)
+        return fname[3:] #for delete 'net'
 
 class InvalidUrl(Exception):
     pass
--- NaverBlogCrawlerTest.py
+++ NaverBlogCrawlerTest.py
@@ -9,9 +9,10 @@ class NaverBlogCrawlerTest(unittest.TestCase):
         del self.crawler
 
     def test_getPostTestUrl(self):
-        postFrameUrl = self.crawler.getPostFrameUrl()
+        postFrameUrl = self.crawler.postFrameUrl
         postFrameUrlAnswer = "https://blog.naver.com/PostView.nhn?blogId=1net1&logNo=221156999402&redirect=Dlog&widgetTypeCall=true&directAccess=false"
         self.assertEqual(postFrameUrl, postFrameUrlAnswer)
+
 
     def test_getEditAreas(self):
         editAreas = self.crawler.getPostEditAreas()
@@ -22,7 +23,22 @@ class NaverBlogCrawlerTest(unittest.TestCase):
     def test_isTextArea(self):
         editAreas = self.crawler.getPostEditAreas()
         targetArea = editAreas[1]
-        self.assertTrue(NaverBlogCrawler.isTextEditArea(targetArea))
+        self.assertTrue(self.crawler.isTextEditArea(targetArea))
+
+    def test_saveImage(self):
+        rawImageUrl = "https://postfiles.pstatic.net/MjAxNzEyMDZfMjc0/MDAxNTEyNTUyNjg1ODQy.Vrm2uKClcVnEsv3i6OLnUKGahND8LDi_mTwRRyWLM8Ig.hveTy2ZjwXir4gaxelXKMRRBvgVR_G0EBrEgMoF2oRgg.PNG.1net1/image.png?type=w966"
+        answerSaveImageName = "MjAxNzEyMDZfMjc0MDAxNTEyNTUyNjg1ODQy.Vrm2uKClcVnEsv3i6OLnUKGahND8LDi_mTwRRyWLM8Ig.hveTy2ZjwXir4gaxelXKMRRBvgVR_G0EBrEgMoF2oRgg.PNG"
+        saveImageName = self.crawler.getSaveImageName(rawImageUrl)
+        self.assertEqual(answerSaveImageName, saveImageName)
+
+    def test_getPostNumber(self):
+        answerPostNumber = "221156999402"
+        postNumber = self.crawler.getPostNumber()
+        self.assertEqual(answerPostNumber, postNumber)
+
+    def test_getPostTitle(self):
+        answerTitle = "[자료구조] 트라이 (Trie)"
+        self.assertEqual(answerTitle, self.crawler.postTitle)
 
 if __name__ == "__main__":
     unittest.main()
